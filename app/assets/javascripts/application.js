3cb3b04949c6161724d127fa56cfe9f5f0f87405
freemind13 <mishclash@gmail.com>
2015-07-22 12:16:28 +0300

    added jquery masonry

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,8 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require jquery.turbolinks
 //= require bootstrap-sprockets
 //= require turbolinks
+//= require masonry/jquery.masonry
 //= require_tree .
